46ff5fb6e16e7fef9affeaf56897850b88908633
Carlos Varela <cvarela@paystand.com>
2024-04-10 17:09:39 -0600

    Lesson 80

---
+++ Udemy/02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  let [playerName, setPlayerName] = useState(initialName);
+  let [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            defaultValue={initialName}
+            onChange={handleChange}
+          />
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
